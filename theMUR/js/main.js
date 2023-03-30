db8b1a2fd3e1a04eead0b3dc7b51d1bc269ca92a
Mathias KLIEM <kliem.mathias@gmail.com>
2023-03-30 20:07:56 +0200

    Mise en place du bon fond et detection du résultat

--- theMUR/js/main.js
+++ theMUR/js/main.js
@@ -1,48 +1,60 @@
+/******
+ * This is the main JS file for the game the M.U.R
+ */
+
+/********
+ * Function to create a fullscreen button
+ */
 document.querySelector(".fullscreen button").addEventListener("click", function () {
     document.documentElement.requestFullscreen();
     document.querySelector(".fullscreen").classList.add("fullscreenOff");
 });
 
-/*document.querySelector("#good").addEventListener("touchmove", function () {
-    console.log("good touched");
-});
-
-document.querySelector("#notGoodInterior").addEventListener("touchmove", function () {
-    console.log("not good touched");
-});*/
-
+/********
+ * Declaration of the canvas and the context of the canvas (2D) and the size of the canvas (full screen)
+ */
 let canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
 
 let tailleX = window.innerWidth;
 let tailleY = window.innerHeight;
 
-
+/********
+ * Function to set the size of the canvas
+ */
 function tailleCanvas() {
     document.querySelector("canvas").width = tailleX;
     document.querySelector("canvas").height = tailleY;
 }
 
+/********
+ * Function to draw a line from the touch input on the canvas
+ * Also check if the touch input is on the good part of the canvas
+ */
+
 // Set the starting position of the line
 let startX = 0;
 let startY = 0;
 
+// Array to store the quality of the touch input
+let touchPoints = [];
+
 document.querySelector("body").addEventListener("touchmove", function (event) {
+    // Set the touch input into variable
     let clientX = event.touches[0].clientX;
     let clientY = event.touches[0].clientY;
-    //console.log(clientX, clientY);
-    let touchedElem = document.elementFromPoint(clientX, clientY).id;
-    //console.log(touchedElem);
-    if (touchedElem && touchedElem === "good") {
-        console.log("good touched");
+    // Check if the touch input is on the good part of the canvas
+    if (document.elementFromPoint(clientX, clientY).classList.contains("borderLimit")) {
+        // If the touch input is on the good part of the canvas, push "good" into the array
+        touchPoints.push("good");
     } else {
-        console.log("bad touched");
+        // If the touch input is not on the good part of the canvas, push "bad" into the array
+        touchPoints.push("bad");
     }
-    //document.elementFromPoint(event.clientX, event.clientY)
 
     /*****
      * Creation of the line from the touch input
-     */
+     *****/
 
         // Get the touch position
     let touch = event.touches[0];
@@ -50,7 +62,6 @@ document.querySelector("body").addEventListener("touchmove", function (event) {
 
     // Draw a line from the starting position to the current touch position
     ctx.lineTo(touch.pageX - canvas.offsetLeft, touch.pageY - canvas.offsetTop);
-
     // Set the line color and width
     ctx.strokeStyle = "green";
     ctx.lineWidth = 4;
@@ -58,6 +69,35 @@ document.querySelector("body").addEventListener("touchmove", function (event) {
     // Draw the line
     ctx.stroke();
 });
+
+/********
+ * Function to check if the touch input is on the good part of the canvas
+ * If 80% of the touch input is on the good part of the canvas, return true
+ * Else return false
+ * @returns {boolean}
+ */
+function checkTouchPoints() {
+    let good = 0;
+    let bad = 0;
+    for (let i = 0; i < touchPoints.length; i++) {
+        if (touchPoints[i] === "good") {
+            good++;
+        } else {
+            bad++;
+        }
+    }
+    //if 80% of the touch points are good return true
+    if (good > (touchPoints.length / 100) * 80) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/********
+ * Function to start a new line from the touch input
+ * Move the drawing cursor to the starting position
+ */
 
 document.querySelector("body").addEventListener("touchstart", function (event) {
     // Get the touch position
@@ -71,54 +111,18 @@ document.querySelector("body").addEventListener("touchstart", function (event) {
     ctx.moveTo(startX, startY);
 });
 
-/*document.querySelector("body").addEventListener("touchmove", function (event) {
-    if (event.target.id === "good" || event.target.id === "notGoodInterior") return;
-    console.log("body touched");
-    event.stopPropagation();
-});*/
-
-/*
-canvas.addEventListener("touchstart", function (event) {
-    // Get the touch position
-    let touch = event.touches[0];
-
-    // Set the starting position of the line
-    startX = touch.pageX;
-    startY = touch.pageY;
-
-    // Move the drawing cursor to the starting position
-    ctx.moveTo(startX, startY);
-});
-
-canvas.addEventListener("touchmove", function (event) {
-    // Get the touch position
-    let touch = event.touches[0];
-    //console.log(touch.pageX, touch.pageY)
-
-    // Draw a line from the starting position to the current touch position
-    ctx.lineTo(touch.pageX - canvas.offsetLeft, touch.pageY - canvas.offsetTop);
-
-    // Set the line color and width
-    ctx.strokeStyle = "green";
-    ctx.lineWidth = 4;
-
-    // Draw the line
-    ctx.stroke();
-});
-*/
-
+/********
+ * Function to resize automatically the canvas when the window is resized
+ * Set the new size of the canvas
+ * Call the function to set the size of the canvas
+ */
 window.addEventListener("resize", () => {
     tailleX = window.innerWidth;
     tailleY = window.innerHeight;
     tailleCanvas();
 });
 
-tailleCanvas();
-
-
-/*
-For fullscren
-window.addEventListener("touchend",function(){
-  document.documentElement.requestFullscreen();
-});
+/********
+ * Call the function to set the size of the canvas for the first time
  */
+tailleCanvas();
