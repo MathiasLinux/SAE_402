f237ce64d657ed556ed0ddb4c2d0f8407c019d6a
Mathias KLIEM <kliem.mathias@gmail.com>
2023-04-03 21:25:39 +0200

    Début de la map avec la géoloc

---
+++ main/js/geoloc.js
@@ -0,0 +1,23 @@
+console.log('geoloc.js loaded');
+
+
+const map = L.map('map').fitWorld();
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 25,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+function onLocationFound(e) {
+    const locationMarker = L.marker(e.latlng).addTo(map)
+
+}
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationfound', onLocationFound);
+map.on('locationerror', onLocationError);
+
+map.locate({setView: true, maxZoom: 20});
