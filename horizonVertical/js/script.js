14b688c33d786773eeb0c89024014e21289fd28e
Benvalda <107179283+Benvalda@users.noreply.github.com>
2023-04-12 17:02:51 +0200

    Update Horizon Vertical
    Normalement c'est bon

--- horizonVertical/js/script.js
+++ horizonVertical/js/script.js
@@ -26,28 +26,56 @@ let Lsol = [
         {w : 0, t : 60, l : 30},
         {w : 0, t : 70, l : 55},
         {w : 10, t : 80, l : 90}
+    ],
+    [
+        {w : 20, t : 20, l : 0},
+        {w : 0, t : 40, l : 20},
+        {w : 0, t : 60, l : 20},
+        {w : 20, t : 80, l : 80}
+    ],
+    [
+        {w : 20, t : 20, l : 0},
+        {w : 0, t : 40, l : 20},
+        {w : 0, t : 60, l : 20},
+        {w : 0, t : 80, l : 20},
+        {w : 0, t : 40, l : 40},
+        {w : 0, t : 60, l : 40},
+        {w : 0, t : 80, l : 40},
+        {w : 0, t : 40, l : 60},
+        {w : 0, t : 60, l : 60},
+        {w : 0, t : 80, l : 60},
+        {w : 20, t : 80, l : 80}
     ]
 ]
 
 let Psol = [
     [
-        {w : 10, taken : false},
-        {w : 20, taken : false},
-        {w : 30, taken : false}
+        {id : 1, w : 10, taken : false},
+        {id : 2, w : 20, taken : false},
+        {id : 3, w : 30, taken : false}
     ],
     [
-        {w : 10, taken : false},
-        {w : 20, taken : false},
-        {w : 30, taken : false}
+        {id : 1, w : 10, taken : false},
+        {id : 2, w : 20, taken : false},
+        {id : 3, w : 30, taken : false}
     ],
     [
-        {w : 10, taken : false},
-        {w : 20, taken : false},
-        {w : 30, taken : false}
+        {id : 1, w : 10, taken : false},
+        {id : 2, w : 20, taken : false},
+        {id : 3, w : 30, taken : false}
+    ],
+    [
+        {id : 1, w : 50, taken : false}
+    ],
+    [
+        {id : 1, w : 20, taken : false},
+        {id : 2, w : 20, taken : false},
+        {id : 3, w : 20, taken : false}
     ]
 ]
 
-let addSolV = 0;
+let addSolV;
+let idPsol;
 
 let sol;
 let solG = [];
@@ -86,9 +114,15 @@ function play() {
     J.style.opacity = "0%";
 }
 
+function clean(){
+    document.querySelector("button.go").classList.toggle("hidden");
+    document.querySelectorAll(".addSol").forEach(aS =>(
+        aS.classList.toggle("hidden")
+    ));
+    document.querySelector(".menuSol").classList.toggle("hidden");
+}
+
 function go(){
-    // console.log(jx,jy)
-    
     if(jx>=(w - jw))
         return next_nv();
     if(jy>=h)
@@ -106,14 +140,14 @@ function getSol(){
 
     Lsol[nv].forEach(Ls => {
         if(Ls.w != 0)
-            document.querySelector(".sols").innerHTML+="<div class='sol' style='width:"+Ls.w+"%; top:"+Ls.t+"%; left:"+Ls.l+"%;' id='"+Ls.l+"'></div>"
+            document.querySelector(".sols").innerHTML+="<div class='sol' style='width:"+Ls.w+"%; top:"+Ls.t+"%; left:"+Ls.l+"%;''></div>"
         else
-            document.querySelector(".sols").innerHTML+="<div class='addSol' style='top:"+Ls.t+"%; left:"+Ls.l+"%;' id='"+Ls.l+"'><img src='img/plus.svg' alt='Add a platform image'></div>"
+            document.querySelector(".sols").innerHTML+="<div class='addSol' style='top:"+Ls.t+"%; left:"+Ls.l+"%;' id='"+Ls.l+"/"+Ls.t+"'><img src='img/plus.svg' alt='Add a platform image'></div>"
     })
 
     Psol[nv].forEach(Ps =>{
         if(Ps.taken==false)
-            document.querySelector(".menuSol>.sols").innerHTML+="<div class='sol' style='width:"+Ps.w+"%;' id='"+Ps.w+"'></div>";
+            document.querySelector(".menuSol>.sols").innerHTML+="<div class='sol' style='width:"+Ps.w+"%;' id='"+Ps.id+"/"+Ps.w+"'></div>";
     })
 
     solG = [];
@@ -125,8 +159,6 @@ function getSol(){
         solD.push(parseInt(s.getBoundingClientRect().x.toFixed()) + parseInt(s.getBoundingClientRect().width.toFixed()));
         soly.push(parseInt(s.getBoundingClientRect().y.toFixed()) - jh);
     })
-
-    // console.log(solG, solD, soly)
 
     document.querySelectorAll(".addSol").forEach(aS => {
         aS.addEventListener("touchstart",openMenu)
@@ -142,22 +174,25 @@ function getCollision(){
     for(let i=0; i<soly.length; i++)
         if(((jx + jw)>=solG[i] && jx<=solD[i]) && jy==soly[i])
             collision=true
-    // console.log(collision)
     return collision;
 }
 
 function next_nv(){
     nv++;
+    if(nv==nvmax){
+        document.cookie = "text=17; path=/"; //17 C'est ce qu'il faut pour horizonVertical
+        location.href = "/main/map.html";
+    }
     document.querySelector(".sols").innerHTML="";
     j.style.transform = "translate(0px,0px)";
     jx = parseInt(j.getBoundingClientRect().x.toFixed());
     jy = parseInt(j.getBoundingClientRect().y.toFixed());
+    clean();
     getSol();
 }
 
 function openMenu(){
     addSolV = this.id;
-    // console.log(addSolV);
     document.querySelector(".menuSol").classList.add("open");
 }
 
@@ -166,16 +201,19 @@ function closeMenu(){
 }
 
 function addSol(){
+    addSolV = addSolV.split("/");
+    idPsol = this.id.split("/");
     Lsol[nv].forEach(Ls =>{
-        if(Ls.l==addSolV)
-            Ls.w = this.id;
+        if(Ls.l==addSolV[0] && Ls.t==addSolV[1])
+            Ls.w = idPsol[1];
     })
 
     Psol[nv].forEach(Ps => {
-        if(Ps.w==this.id)
+        if(Ps.id==idPsol[0])
             Ps.taken = true;
     })
 
+    closeMenu();
     getSol();
 }
 
@@ -185,6 +223,7 @@ document.querySelector(".jouer>button").addEventListener("touchstart",play)
 
 resizeWindow();
 
+document.querySelector("button.go").addEventListener("touchstart",clean)
 document.querySelector("button.go").addEventListener("touchstart",go)
 
 document.querySelectorAll(".addSol").forEach(aS => {
@@ -196,5 +235,3 @@ document.querySelector(".menuSol>.closeMenu>img").addEventListener("touchstart",
 document.querySelectorAll(".menuSol>.sols>.sol").forEach(mSSS => {
     mSSS.addEventListener("touchstart",addSol)
 })
-
-// document.querySelector("button.nv_next").addEventListener("touchstart",next_nv)
